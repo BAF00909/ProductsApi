9815a9ae163575082bab96f8a25f4dbeea6d5c18
Alexander Breus <Anyuta069@yandex.ru>
2025-04-28 17:27:59 +0300

    add validation for entity fields

--- entities/Product.cs
+++ entities/Product.cs
@@ -5,7 +5,11 @@ namespace ProductsApi.entities
     public class Product
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
         public decimal Price { get; set; }
     }
 }
